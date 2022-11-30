67edff4d901dae231dfa19d78443af38bc65deb6
jarrospide1 <juanarrospidepons@gmail.com>
2022-11-30 11:44:40 -0500

    Added transitions to new classList, and script to execute class toggle

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -3,11 +3,13 @@ console.log(img)
 
 for(let i = 0; i < img.length; i++) {
     img[i].addEventListener("click", () => {
-        img[i].classList.toggle("photo_expanded")
+        removeClass();
+        img[i].classList.toggle("photo_expanded");
     })
 }
 
-// function respondToCLick() {
-//     img.classList.toggle("photo_expanded")
-    
-// }
+function removeClass() {
+    img.forEach(image => {
+        image.classList.remove("photo_expanded")
+    })
+}
