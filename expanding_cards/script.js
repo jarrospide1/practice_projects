1b99205a7a42498ae215e5e7b7d895afef1f71c4
jarrospide1 <juanarrospidepons@gmail.com>
2022-12-01 12:07:17 -0500

    dark mode added

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,5 +1,4 @@
 const img = document.querySelectorAll(".image");
-console.log(img)
 
 for(let i = 0; i < img.length; i++) {
     img[i].addEventListener("click", () => {
@@ -10,6 +9,25 @@ for(let i = 0; i < img.length; i++) {
 
 function removeClass() {
     img.forEach(image => {
-        image.classList.remove("photo_expanded")
+        image.classList.remove("photo_expanded");
     })
 }
+
+// Dark Mode
+const darkModeButton = document.querySelector(".theme-button ");
+const body = document.querySelector("body");
+const nightPhoto = document.querySelectorAll(".night-photo");
+const dayPhoto = document.querySelectorAll(".day-photo");
+const label = document.querySelector("label");
+
+function themeMode() {
+    label.addEventListener("click", () => {
+        body.classList.toggle("night-background");
+        body.classList.toggle("ligth-background");
+        for(let i = 0; i < nightPhoto.length; i++) {
+            nightPhoto[i].classList.toggle("hide");
+            dayPhoto[i].classList.toggle("hide");
+        }
+    })
+}
+themeMode();
